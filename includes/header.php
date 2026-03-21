a3314e2624166783be4862a04d3be4dc5db32b80
Zenith Kandel <kandelze123@gmail.com>
2026-03-21 16:56:02 +0545

    Update font styles in header.php to use Inter and Plus Jakarta Sans

--- includes/header.php
+++ includes/header.php
@@ -36,9 +36,7 @@ $clinicAddress = getSiteSettingValue('clinic_address', CLINIC_ADDRESS);
     <link rel="stylesheet" href="<?php echo SITE_URL; ?>CSS/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500;9..144,700&family=Manrope:wght@400;500;600;700;800&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Plus+Jakarta+Sans:wght@500;600;700;800&display=swap" rel="stylesheet">
 </head>
 
 <body>
